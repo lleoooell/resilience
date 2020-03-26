6a8e21c192b72e5689ab48576d0dccab5472b136
Olivier Veinand <olivier.veinand@gmail.com>
2020-03-26 16:00:49 +0100

    sqlite rest api

--- server.js
+++ server.js
@@ -2,6 +2,33 @@ var express = require('express'); // Get the module
 var app = express(); // Create express by calling the prototype in var express
 var http = require('http').createServer(app);
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('resilience');
+
+
+app.get("/besoins", (req, res) => {
+  const sql = "SELECT * FROM besoins";
+  db.all(sql, (err, result) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.json(result);
+  });
+});
+
+
+app.post("/besoins", (req, res) => {
+    var params=req.body;
+    const sql = "INSERT INTO besoins (Nom,Addresse,Lat,Lng,MasquesFFP2,MasquesChir,Blouses,Gel,Autre,Comentaire,Contact) VALUES ("+params.Nom+","+params.Addresse+","+params.Lat+","+params.Lng+","+params.MasquesFFP2+","+params.MasquesChir+","+params.Blouses+","+params.Gel+","+params.Autre+","+params.Comentaire+","+params.Contact+")";
+    db.run(sql, function(err) {
+        if (err) {
+        return console.log(err.message);
+        res.json(err.message);
+        }
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+        res.json("ok");
+    });
+});
 
 
 app.get('/', function(req, res){
